4951c6f3ae166343ebc7fe0bf5fa19ffc05eff7e
Panikd Kernel <panikdkernel@gmail.com>
2018-02-19 23:06:38 +0530

    Add : Routing (issues)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [RouterModule.forRoot(routes)]
+})
+
+export class AppRoutingModule {}
+
