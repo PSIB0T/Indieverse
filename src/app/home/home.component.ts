02389ecd161e96892a483890338b391a4e0d96ad
Arvind Narayanan <arvindonarayanan@gmail.com>
2018-02-27 07:18:47 +0530

    Update: Home page sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  artists = [{
+    name: 'DJ Khaled',
+    image: 'https://images.complex.com/complex/image/upload/c_fill,f_auto,g_faces:auto,h_900,q_auto,w_900/m6mjx9nhyk72xtqj3e2p.jpg',
+    sub_count: 13232
+  }, {
+    name: 'Ed Sheeran',
+    image: 'https://images.shazam.com/artistart/a40769350_s400.jpg',
+    sub_count: 324143
+  }, {
+    name: 'Britney Spears',
+    image: 'https://pbs.twimg.com/profile_images/955803874463571969/-R8etznz_400x400.jpg',
+    sub_count: 306144
+  }]
   constructor() { }
 
   ngOnInit() {
