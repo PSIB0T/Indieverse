e33e978215453c024e77c857922c517d1d237341
Arvind Narayanan <arvindonarayanan@gmail.com>
2018-03-13 11:46:05 +0530

    Update: Flexible nav

--- src/app/dashboard/browse/browse.component.ts
+++ src/app/dashboard/browse/browse.component.ts
@@ -11,6 +11,7 @@ import { Component,
 })
 export class BrowseComponent implements OnInit {
 
+  songs = ['Circus', 'Somewhere I belong', 'Numb', 'In the end'];
   @ViewChild('browserNav', {read: ElementRef}) browser_nav: ElementRef;
   constructor() { }
 
--- src/app/dashboard/nav/nav.component.ts
+++ src/app/dashboard/nav/nav.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  isHam = true;
+  class_name = 'fa fa-bars ham'
   constructor() { }
 
   ngOnInit() {
+  }
+  toggleHam() {
+    if (this.isHam) {
+      this.class_name = 'fa fa-times ham';
+      this.isHam = false;
+    } else {
+      this.class_name = 'fa fa-bars ham';
+      this.isHam = true;
+    }
   }
 
 }
