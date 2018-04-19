1b36a9598f1f665f687a879172d5bd397073a12a
Arvind Narayanan <arvindonarayanan@gmail.com>
2018-04-19 07:41:10 +0530

    Update: config

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ app.post('/uploadFile', upload.single('fileName'), (req, res) => {
 });
 // Start the app by listening on the default
 // Heroku port
-app.listen(config.port, () => {
+
+let port = process.env.PORT || 8080
+app.listen(port, () => {
   console.log("Server is running!");
 });
